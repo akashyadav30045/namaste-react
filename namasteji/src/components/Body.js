c642ef39bb27d29cbcfc775261f268e1bb0dc419
Akash yadav <119119145+akashyadav30045@users.noreply.github.com>
2023-10-26 12:03:44 +0530

    ep-5 lets get hooked

--- namasteji/src/components/Body.js
+++ namasteji/src/components/Body.js
@@ -1,13 +1,35 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard';
 import restaurantList from './utils/mockData';
+import { useState } from 'react';
+
+// state hook
+ 
 
 const Body = () => {
+  const[listofRestaurants,setlistofRestaurants] = useState(restaurantList);
     return (
-      <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
-          return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
-        })}
+      <div className="body">
+        <div className="filter">
+          <button className="filter-btn"  
+          onClick={  () => {
+            const filteredlist = listofRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setlistofRestaurants(filteredlist);
+            console.log("hello")
+            }}
+          >
+          
+            Top Rated Restaurants
+          </button>
+
+        </div>
+          <div className="restaurant-list">
+            {restaurantList.map((restaurant) => {
+              return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+            })}
+      </div>
       </div>
     );
   };
